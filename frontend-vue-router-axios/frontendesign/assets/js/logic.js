1a422a66c5c377fa810b551647f9b8d19ae6a859
Marv3 <marvin4alarcon3@gmail.com>
2020-10-03 10:27:24 -0600

    Agregado Vue Cli, configurado componente principal para el menu

--- frontend-vue-router-axios/frontendesign/assets/js/logic.js
+++ frontend-vue-router-axios/frontendesign/assets/js/logic.js
@@ -1,6 +1,8 @@
 document.getElementById('restaurantes').addEventListener('click',showRestaurants);
 document.getElementById('carrito').addEventListener('click',showShopingCart);
 document.getElementById('cart-icon').addEventListener('click',showShopingCart);
+document.getElementById('log-out-pc-icon').addEventListener('click',showLogin);
+document.getElementById('log-out-icon').addEventListener('click',showLogin);
 
 
 function showRestaurants() {
@@ -34,5 +36,25 @@ function showShopingCart() {
         document.getElementById('restaurantes').className = 'nav__link';
         document.getElementById('title').innerHTML = "<h1 class='title-shop' style='margin-top: 100px;'>Mi Pedido</h1>";
 
+}
+
+function showLogin() {
+        loginhtml = "<div class='card-login center'>" +
+                "<h2 class='title'>INICIAR SESIÓN</h2>"
+                        "<div class='content'>" +
+                        "<form action='' class='form-class'>" +
+                        "<div>" +
+                            "<label for='user'>Usuario</label>" +
+                            "<input type='text' name='user' id='user'>" +                                       "</div>" +
+                        "<div>" +
+                        "<label for='user'>Contraseña</label>" +                                            "<input type='password' name='password' id='password'>" +
+                        "</div>"
+                        "</form>" +
+                        "</div>" +
+                        "</div>";
+        document.getElementById('main').innerHTML = loginhtml;
+        document.getElementById('title').innerHTML = "";
+
+                                
 }
 
---
+++ frontend-vue-router-axios/restaurants-vue-project/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <header class="header">
+            <a href="#" class="header__logo">RUEDAS</a>
+            <a href="#" class="nav__social-icon"><i class='bx bx-search-alt' id="search-icon"></i></a>
+            <a href="#" class="nav__social-icon"><i class='bx bx-cart-alt' id="cart-icon"></i></a>
+            <i class='bx bx-menu header__toggle menu-icon' @click="showMenu" id="nav-toggle"
+                style='color:#e2d8d8;font-size: 18px;'></i>
+
+            <nav class="nav" ref='navMenu' id="nav-menu">
+                <div class="nav__content bd-grid">
+
+                    <i class='nav-close bx bxs-x-circle' @click="closeMenu()" style='color:#f5ecec; text-align: right; font-size: 25px;' id="nav-close"></i>
+                    <div class="nav__perfil">
+                        <div>
+                            <a href="#" class="nav__name">RUEDAS</a>
+                            <span class="nav__profesion">App de Entregas</span>
+                        </div>
+                    </div>
+                    <div class="nav__menu">
+                        <ul class="nav__list">
+                            <li class="nav__item"><a href="#" id="restaurantes"  @click="linkAction(1)" :class="{active:selected == 1}" class="nav__link restaurantes">Restaurantes</a></li>
+                            <li class="nav__item"><a href="#" id="carrito" @click="linkAction(2)" :class="{active:selected == 2}" class="nav__link">Carrito</a></li>
+                            <li class="nav__item"><a href="#" @click="linkAction(3)" :class="{active:selected == 3}" class="nav__link">Checkout</a></li>
+                            <li class="nav__item"><a href="#"  class="nav__social-icon"><i class='bx bx-log-out' id="log-out-icon"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="nav__social">
+                        <a href="#" class="nav__social-icon"><i class='bx bx-search-alt' id="search-pc-icon"></i></a>
+                        <a href="#" class="nav__social-icon"><i class='bx bx-cart-alt' id="cart-pc-icon"></i></a>
+                        <a href="#" class="nav__social-icon"><i class='bx bx-log-out' id="log-out-pc-icon"></i></a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+  data() {
+         return {
+             selected: undefined
+         }
+    },
+  methods: {
+      /*SHOW MENU*/
+        showMenu() {
+          this.$refs.navMenu.classList.toggle('show')
+        },
+        /*HIDDEN*/
+        closeMenu() {
+            console.log("sdf");
+            this.$refs.navMenu.classList.remove('show');
+        },
+        linkAction(item) {
+            this.selected = item; 
+            this.$refs.navMenu.classList.remove('show')
+        }
+    /*===== ACTIVE AND REMOVE MENU =====*/
+    // const navLink = document.querySelectorAll('.nav__link'); 
+
+}
+}
+</script>
