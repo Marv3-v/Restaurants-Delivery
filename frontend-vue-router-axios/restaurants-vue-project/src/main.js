5a40da3825c391ff62b11eae0ef581b1a2d664d1
Marv3 <marvin4alarcon3@gmail.com>
2020-10-05 20:57:49 -0600

    Agregado paquete de Vuex para manejo del Carrito

--- frontend-vue-router-axios/restaurants-vue-project/src/main.js
+++ frontend-vue-router-axios/restaurants-vue-project/src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
-import router from './router'
-import "./assets/css/styles.css"
+import router from './router';
+import { store } from './store/store.js';
+import "./assets/css/styles.css";
 import "./assets/css/articles.css";
 import "./assets/icons/css/boxicons.min.css";
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store: store,
   render: h => h(App),
 }).$mount('#app')
