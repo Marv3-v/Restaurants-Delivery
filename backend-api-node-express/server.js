db973f6505a374a3c65b0f3db57f0beb6e829eec
Marv3 <marvin4alarcon3@gmail.com>
2020-10-07 13:40:08 -0600

    Actualizado puerto para evitar CORS

--- backend-api-node-express/server.js
+++ backend-api-node-express/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8082"
+    origin: "http://localhost:8083"
 };
 
 app.use(cors(corsOptions));
