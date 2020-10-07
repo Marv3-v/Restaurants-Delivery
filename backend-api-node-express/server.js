8dd7ca65753819590c219cc6c46d141a03d84c4c
Marv3 <marvin4alarcon3@gmail.com>
2020-10-07 15:04:06 -0600

    Modificado puerto de lectura backend

--- backend-api-node-express/server.js
+++ backend-api-node-express/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8083"
+    origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
